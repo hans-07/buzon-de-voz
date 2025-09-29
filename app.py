8447c2a4ca321fcec1f171c2b9eef9eb4e190c39
Hans <hansparrahuichaqueo@outlook.com>
2025-09-29 09:48:59 -0300

    rutas modificadas

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, render_template, session, redirect, request, flash, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#Ruta para el login unificado
+@app.route('/login')
+def login_unificado():
+    return render_template('login_unificado.html')
+    
 @app.route('/reporte')
 def hacer_reporte():
-    return render_template('/reporte.html')
+    return render_template('reporte.html')
 
 @app.route('/admin')
 def admin():
