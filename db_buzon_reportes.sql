918a9dc2cef2e7f4a75df7afd327f2d3d5ac9fce
Axel Pincheira <teck007@gmail.com>
2025-10-27 18:05:52 -0300

    fix: ensure database creation and selection before dropping reportes table

--- db_buzon_reportes.sql
+++ db_buzon_reportes.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `reportes`
 --
-
+CREATE DATABASE IF NOT EXISTS `db_buzon`;
+USE db_buzon;
 DROP TABLE IF EXISTS `reportes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
